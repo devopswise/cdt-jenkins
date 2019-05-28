8f3a7fc27112c81c48cd1835210f62b0a643a4b1
Onur Ozkol <onur@devopswise.co.uk>
2019-05-28 17:41:27 +0100

    update docker slave config to user jnlp

--- init.groovy.d/docker-slave-config.groovy
+++ init.groovy.d/docker-slave-config.groovy
@@ -9,10 +9,11 @@
 */
 import jenkins.model.*;
 import hudson.model.*;
+import hudson.slaves.*
 import com.nirima.jenkins.plugins.docker.DockerCloud
 import com.nirima.jenkins.plugins.docker.DockerTemplate
 import com.nirima.jenkins.plugins.docker.DockerTemplateBase
-import io.jenkins.docker.connector.DockerComputerAttachConnector
+import io.jenkins.docker.connector.DockerComputerJNLPConnector
 import org.jenkinsci.plugins.docker.commons.credentials.DockerServerEndpoint
 
 
@@ -25,10 +26,9 @@ def image = env['DOCKER_SLAVE_IMAGE']
 def dockerSlaveLabel = env['DOCKER_SLAVE_LABEL']
 def dockerHostUri = env['DOCKER_SLAVE_DOCKER_HOST_URI']
 
-
 // https://github.com/jenkinsci/docker-plugin/blob/docker-plugin-1.1.2/src/main/java/com/nirima/jenkins/plugins/docker/DockerTemplateBase.java#L122
 DockerTemplateBase templateBase = new DockerTemplateBase(
-      						  image, // image
+                                image, // image
                               null, // pullCredentialsId
                               null, // dnsString
                               network, // network
@@ -48,7 +48,10 @@ DockerTemplateBase templateBase = new DockerTemplateBase(
                               "" // extraHostsString
 );
 
-DockerComputerAttachConnector connector = new DockerComputerAttachConnector("root")
+jlc = JenkinsLocationConfiguration.get()
+
+DockerComputerJNLPConnector connector = new DockerComputerJNLPConnector(new JNLPLauncher(null, null)).withUser("jenkins")
+                    .withJenkinsUrl(jlc.jenkinsUrl);
 
 DockerTemplate dkTemplate = new DockerTemplate(templateBase,connector,dockerSlaveLabel,"","");
 
@@ -59,17 +62,17 @@ DockerServerEndpoint endpoint = new DockerServerEndpoint(dockerHostUri, "")
 
 ArrayList<DockerCloud> dkCloud = new ArrayList<DockerCloud>();
 dkCloud.add(
-		// https://github.com/jenkinsci/docker-plugin/blob/docker-plugin-1.1.2/src/main/java/com/nirima/jenkins/plugins/docker/DockerCloud.java#L106
-		new DockerCloud(
-				"docker",
-				dkTemplates,
-				endpoint,
-				2,
-				60,
-				60,
-				"",
-				""
-			)
+        // https://github.com/jenkinsci/docker-plugin/blob/docker-plugin-1.1.2/src/main/java/com/nirima/jenkins/plugins/docker/DockerCloud.java#L106
+        new DockerCloud(
+                "docker",
+                dkTemplates,
+                endpoint,
+                2,
+                60,
+                60,
+                "",
+                ""
+            )
 );
 
 Jenkins.getInstance().clouds.replaceBy(dkCloud)
